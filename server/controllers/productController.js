66ee2f290b7fd59dfb7a40220c4b37cc1a72a936
ihzaw <ihzanantama@gmail.com>
2022-02-25 21:02:36 +0800

    last server commit

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,16 +4,16 @@ const createTimestampId = require('../helpers/createTimestampId')
 
 const addProduct = async (req, res, next) => {
   try {
-    const { name, price, stock } = req.body;
-    const { seller_code } = req.params;
+    const { name, price, stock, imageUrl } = req.body;
+    const { seller_uuid } = req.params;
     const id = createTimestampId()
 
     if (!name) throw { name: "BAD_REQUEST", message: "Name is required" };
     if (!price) throw { name: "BAD_REQUEST", message: "Price is required" };
-    if (!seller_code)
+    if (!seller_uuid)
       throw { name: "BAD_REQUEST", message: "Seller uuid is required" };
 
-    const response = await Product.create({id, ...req.body, seller_code });
+    const response = await Product.create({id, ...req.body, seller_uuid });
 
     const payload = generatePayload(response, "Success");
     res.status(201).json(payload);
--- server/helpers/createTimestampId.js
+++ server/helpers/createTimestampId.js
@@ -1,7 +1,6 @@
 const createTimestampId = () => {
     let now  = new Date()
     let result = now.getMonth().toString() + now.getDate().toString() + now.getHours().toString() + now.getMinutes().toString() + now.getSeconds().toString()
-    console.log(result , parseInt(result))
     return parseInt(result)
 }
 
