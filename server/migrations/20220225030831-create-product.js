f079ded7f2a63f32242297b8cc453a96064dbf0c
ihzaw <ihzanantama@gmail.com>
2022-02-25 11:20:01 +0800

    add sequelize product table migration

---
+++ server/migrations/20220225030831-create-product.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        defaultValue: new Date().getTime()
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
