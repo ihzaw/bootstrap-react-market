16ae6d84d21141da4c535963dcc50ecd1ea27323
ihzaw <ihzanantama@gmail.com>
2022-02-25 11:30:08 +0800

    add seller table migration

---
+++ server/migrations/20220225032158-create-seller.js
@@ -0,0 +1,33 @@
+'use strict';
+const uuid4 = require('uuid4')
+console.log(uuid4())
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Sellers', {
+      seller_code: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: uuid4()
+      },
+      seller_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Sellers');
+  }
+};
