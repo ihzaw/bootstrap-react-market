dcd1b7eab14ff6d361fdceffaa1f659d44ccda84
ihzaw <ihzanantama@gmail.com>
2022-02-25 15:56:39 +0800

    add update-seller endpoint, add delete-seller endpoint

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -20,7 +20,15 @@ const addSeller = async (req, res, next) => {
 
 const editSeller = async (req, res, next) => {
     try {
-        
+        const { seller_code } = req.params
+        if (!req.body) throw { name: 'BAD_REQUEST' }
+        if (!seller_code) throw { name: "BAD_REQUEST", message: 'Seller uuid is required'}
+
+        const [success, data] = await Seller.update(req.body, { where: { seller_code }, returning: true, raw: true })
+        if (!success) throw err
+
+        const payload = generatePayload(data[0], 'Success')
+        res.status(200).json(payload)
     } catch (err) {
         next(err)
     }
@@ -28,7 +36,13 @@ const editSeller = async (req, res, next) => {
 
 const deleteSeller = async (req, res, next) => {
     try {
-        
+        const { seller_code } = req.params
+        if (!seller_code) throw { name: "BAD_REQUEST", message: 'Seller uuid is required'}
+
+        const response  = await Seller.destroy({ where: { seller_code }})
+        if (!response) throw err
+
+        res.status(200).json({ message: 'OK' })
     } catch (err) {
         next(err)
     }
--- server/routes/sellerRouter.js
+++ server/routes/sellerRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { addSeller, editSeller, deleteSeller} = require('../controllers/sellerController')
 
 router.post('/', addSeller)
-router.put('/', editSeller)
-router.delete('/', deleteSeller)
+router.put('/:seller_code', editSeller)
+router.delete('/:seller_code', deleteSeller)
 
 module.exports = router
